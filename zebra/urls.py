9d3ebc4f2d3d04beff1e13d57b099e950bfd62a4
Cat <vince@procurify.com>
2016-09-12 18:22:52 -0700

    remove string in urlpattern

--- zebra/urls.py
+++ zebra/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from zebra import views
 
-urlpatterns = ['',
+urlpatterns = [
     url(r'webhooks/$',     views.webhooks,          name='webhooks'),
     url(r'webhooks/v2/$',     views.webhooks_v2,          name='webhooks_v2'),
 ]
