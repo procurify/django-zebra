e87b7ca7916b545a6ada0c51c2d92adb30909302
Colin Rolfe <crolfe@gmail.com>
2018-05-24 07:39:36 -0700

    remove unused imports

--- zebra/templatetags/zebra_tags.py
+++ zebra/templatetags/zebra_tags.py
@@ -1,17 +1,13 @@
-from django.core.urlresolvers import reverse
 from django import template
-from django.template.loader import render_to_string
-from django.utils.encoding import force_unicode
-from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext as _
 
 from zebra.conf import options
 
 
 register = template.Library()
 
+
 def _set_up_zebra_form(context):
-    if not "zebra_form" in context:
+    if "zebra_form" not in context:
         if "form" in context:
             context["zebra_form"] = context["form"]
         else:
@@ -20,7 +16,8 @@ def _set_up_zebra_form(context):
     return context
 
 
-@register.inclusion_tag('zebra/_stripe_js_and_set_stripe_key.html', takes_context=True)
+@register.inclusion_tag('zebra/_stripe_js_and_set_stripe_key.html',
+                        takes_context=True)
 def zebra_head_and_stripe_key(context):
     return _set_up_zebra_form(context)
 
